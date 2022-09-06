8ffebcc11596d6f786adedfdd0197737f4c53401
rajik1234 <112938543+rajik1234@users.noreply.github.com>
2022-09-06 15:08:50 +0530

    Add files via upload

---
+++ ConuntVCW.java
@@ -0,0 +1,39 @@
+package javapack2;
+
+import java.util.Scanner;
+
+public class ConuntVCW {
+
+	public static void main(String[] args) {
+		System.out.println("Enter the text");
+		Scanner sc= new Scanner(System.in);
+		String s1=sc.nextLine();
+		s1=s1.toLowerCase();
+		char ch[]=s1.toCharArray();
+		int vow=0, cons=0, spaces=0,digits=0;
+		for(int i=0;i<ch.length;++i)
+		{
+			
+			if(ch[i]=='a' || ch[i]=='e' || ch[i]=='i' || ch[i]=='o' || ch[i]=='u') {
+				++vow;
+			}
+			else if(ch[i]>='a' && ch[i]<='z' ) {
+				++cons;
+			}
+			else if(ch[i]>= '0' && ch[i]<='9') {
+				++digits;
+			}
+			else if(ch[i]==' ') {
+				++spaces;
+			}
+		}
+		System.out.println("number of vowels count:"+vow);
+		System.out.println("number of consonants count:"+cons);
+		System.out.println("number of spaces count:"+spaces);
+		System.out.println("number of digits count:"+digits);
+		
+		
+
+	}
+
+}
